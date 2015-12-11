70a06a50cc0b992672b9bac06be50f048beeecc2
bruneniece5 <bruneniece5@inbox.lv>
2015-12-11 10:19:09 +0200

    Pirmais commit

---
+++ Git_simple/Git_simple/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Git_simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Hello world!!!\n\n Press any key to exit!");
+            Console.ReadKey();
+
+        }
+    }
+}
